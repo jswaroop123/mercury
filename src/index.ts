bd9b3db0515ec2284509d19cb64a05ab0a71e5c1
Jyothi Swaroop N <jswaroop2003@gmail.com>
2025-03-13 11:32:36 +0530

    Updated code for POST /reminders

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,30 @@
 import { serve } from '@hono/node-server'
-import { Hono } from "hono";
+import { Hono } from 'hono'
 
-const app = new Hono();
+const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+type Reminder = {
+  id: string,
+  title: string,
+  description: string,
+  dueDate: string,
+  isCompleted: boolean
+}
+
+const reminders: Reminder[] = [];
+
+app.post("/reminder", async (c) => {
+  const body = await c.req.json();
+  const newReminder: Reminder = body.reminder;
+
+  if (newReminder && newReminder.id && newReminder.title && newReminder.description && newReminder.dueDate && typeof newReminder.isCompleted === 'boolean') {
+    reminders.push(newReminder);
+    return c.json(newReminder, 200); // 200 OK
+  } else {
+    return c.json({ error: 'Invalid request' }, 400); // 400 Bad Request
+  }
 })
 
-serve(app)
+serve(app);
+
 console.log(`Server is running on http://localhost:${3000}`)
